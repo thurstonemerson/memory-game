372c5e9faf4729804284a3aaa32f3421a481696d
thurstonemerson <jmt.emerson@gmail.com>
2016-03-31 10:20:53 +1300

    Restructure games service testing module

--- tests/games/test_service.py
+++ tests/games/test_service.py
@@ -1,23 +1,20 @@
 '''
 Created on 30/03/2016
 
+Testing module for running unit tests on public methods from the games service.
+Uses a sqllite database so that the google app engine doesn't need to be running.
+
 @author: Jessica
 '''
 import unittest
+import logging
 from services import games
 from users.models import User
 from games.models import CardNames, Card
 
-from google.appengine.api import memcache
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
-
-class Request:
-    def __init__(self, name):
-        self.name = name
-        
-    def all_fields(self):
-        return 
+ 
 
 class GameTest(unittest.TestCase):
     nosegae_datastore_v3 = True
@@ -27,6 +24,8 @@ class GameTest(unittest.TestCase):
     }
     
     def setUp(self):
+        """Set up the test bed and activate it"""
+        logging.info("Setting up testbed...")
         # First, create an instance of the Testbed class.
         self.testbed = testbed.Testbed()
         # Then activate the testbed, which prepares the service stubs for use.
@@ -38,14 +37,15 @@ class GameTest(unittest.TestCase):
         ndb.get_context().clear_cache()
 
     def test_new_game(self):
-        #games.new_game("", "")
-        #request = Request(name="hello")
+        """Create a new game and test all the default information is correct"""
+        logging.info("Testing game creation")
         first_user = User(name=u'good golly', email=u'generic@thingy.com')
         first_user.put()
         
         second_user = User(name=u'my mummy', email=u'generic@thingy.com')
         second_user.put()
         
+        #create a new game
         game = games.new_game(first_user.key, second_user.key)
         
         #check that the game details were created with correct default values
@@ -76,10 +76,20 @@ class GameTest(unittest.TestCase):
             self.assertEqual(num, 2, "There must be 2 {0} in the card deck, only contains {1}".format(card_name, num))
                     
             
-                    
+    def test_is_valid_move(self):  
+        """Test that valid moves are allowed and invalid moves are not"""     
+        logging.info("Testing is valid move")
         
+    def test_to_form(self):   
+        """Test that a game form can be created"""    
+        logging.info("Testing to form")
         
+    def test_make_move(self):    
+        """Test that a move can be made"""   
+        logging.info("Testing make a move")
         
         
     def tearDown(self):
+        """Tear down the test bed by deactivating it"""
+        logging.info("Tearing down testbed...")
         self.testbed.deactivate()
